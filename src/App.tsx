3aaec46623d82b834813ac46172cb3825cbf1bcc
dhsiao510 <dhsiao510@gmail.com>
2022-07-07 23:48:09 -0700

    renders current month

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Calendar from './components/Calendar';
 import EventDetail from './components/EventDetails';
 
 function App() {
+  const [ today, setToday ] = useState(new Date());
+  const [ showDetail, setShowDetail] = useState<boolean>(false);
+
   return (
     <div className="App">
       <Calendar />
+      { showDetail ? <EventDetail />: <></>}
     </div>
   );
 }
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Calendar = () => {
+    const weekdays: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    const months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const longMonths: string[] = ['January', 'March', 'May', 'July', 'August', 'October', 'December']
+    
+    let today: any = new Date();
+    today = today.toString().split(' ').slice(2,4).reverse().join(' ');
+    const currentMonth: string = months[today.split(' ')[1] - 1];
+    const firstOfMonthIdx: number = new Date(today).getDay();
+    console.log(firstOfMonthIdx)
+    const calGrids = Array(42).fill(null);
+    const numberOfDays: number = longMonths.indexOf(currentMonth) > -1 ? 31:30;
+
+
+
+    const renderHeader = (array:Array<string>) => {
+        return array.map((day:string, key) => <div key={key} className="cal-header">{day}</div>)
+    }
+    const renderGrid = (array: any) => {
+        return array.map((day: any, key:any) => {
+            if(key >= firstOfMonthIdx && key < (firstOfMonthIdx + numberOfDays)) {
+                return <div key={key} className="cal-cel-days" onClick={() =>{console.log(key)}}>{key - firstOfMonthIdx + 1} </div>
+            } else {
+                return <div key={key} className="cal-cel"></div>
+            }
+        }
+    )}
+
+    const selectDate = () => {
+    }
+
 
     return (
-        <>
-            <h1>Calendar</h1>
-        </>
+        <div className="cal-wrapper">
+            <h1>{currentMonth}</h1>
+            <div className="cal-table">
+                {renderHeader(weekdays)}
+                {renderGrid(calGrids)}
+            </div>
+        </div>
     )
 }
 
