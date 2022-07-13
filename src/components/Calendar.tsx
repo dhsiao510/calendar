d34f1bbe67944803522f737377de950d2b0b436d
dhsiao510 <dhsiao510@gmail.com>
2022-07-13 15:26:55 -0700

    minor clean up

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -4,7 +4,6 @@ import currentMonthsEventsToDayMap from "../utils/filterEventsForCurrentMonth";
 import EventDetail from "./EventDetails";
 import { Event } from "../types";
 
-
 const initialSelectDayFormat = (stringDate?: string) => {
     stringDate = stringDate ? stringDate: format(new Date(), "MM/dd/yyyy");
     const arrDate = stringDate.split('/');
@@ -23,6 +22,7 @@ const Calendar = ({events}: any) => {
     const weekdays: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const months: any = {1:'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9:'Sep', 10: 'Oct', 11:'Nov', 12: 'Dec'}
     const [ selectDay, setSelectDay ]= useState(() => initialSelectDayFormat()) as any;
+    const { month, day, year, monthLength, firstOfMonthIdx } = selectDay;
 
     const [ sameMonth, setSameMonth] = useState<boolean>(true); 
     const [ selectEvent, setSelectEvent] = useState(null) as any;
@@ -30,14 +30,11 @@ const Calendar = ({events}: any) => {
     const allEvents = useRef<Event[]>(events);
     const [ currentMonthEvents, setCurrentMonthEvents ] = useState([]) as any;
     
-
-    
     useEffect(() => {
-        setCurrentMonthEvents(currentMonthsEventsToDayMap(allEvents.current,  months[Number(selectDay.month)], selectDay.year));
-        setSameMonth(isSameMonth(new Date(`${selectDay.month}/${selectDay.day}/${selectDay.year}`), new Date()))
+        setCurrentMonthEvents(currentMonthsEventsToDayMap(allEvents.current,  months[Number(month)], year));
+        setSameMonth(isSameMonth(new Date(`${month}/${day}/${year}`), new Date()))
         setToday(Number(format(new Date(), "dd")))
-    
-      }, [selectDay])
+      },[selectDay])
     
     const renderHeader = (array:Array<string>) => {
         return array.map((day:string, key) => <div key={key} className="cal-header">{day}</div>)
@@ -45,14 +42,14 @@ const Calendar = ({events}: any) => {
 
     const renderGrid = (array: any) => {
             return array.map((day: any, key:any) => {
-                if(key >= selectDay.firstOfMonthIdx && key < (selectDay.firstOfMonthIdx + selectDay.monthLength)) {
+                if(key >= firstOfMonthIdx && key < (firstOfMonthIdx + monthLength)) {
                     return (
                     <div role="cell" key={key} className="cal-cel-days" onClick={() => renderEventDetail(key)}>
-                        <div role="cell" className={sameMonth && (today + selectDay.firstOfMonthIdx - 1) === key ? "current": ""}>{key - selectDay.firstOfMonthIdx + 1}</div>
-                        { currentMonthEvents[ key - selectDay.firstOfMonthIdx + 1] ? <div className="event">{currentMonthEvents[key - selectDay.firstOfMonthIdx + 1].name}</div>:<></>}
+                        <div role="cell" className={sameMonth && (today + firstOfMonthIdx - 1) === key ? "current": ""}>{key - firstOfMonthIdx + 1}</div>
+                        { currentMonthEvents[ key - firstOfMonthIdx + 1] ? <div className="event">{currentMonthEvents[key - firstOfMonthIdx + 1].name}</div>:<></>}
                     </div>)
-                } else if(36 > (selectDay.firstOfMonthIdx + selectDay.monthLength) && key > 34){
-                    return <div role="cell" key={key} className="cal-cel-white" onClick={() => console.log(key, selectDay.firstOfMonthIdx + selectDay.monthLength)}></div>
+                } else if(36 > (firstOfMonthIdx + monthLength) && key > 34){
+                    return <div role="cell" key={key} className="cal-cel-white" onClick={() => console.log(key, firstOfMonthIdx + monthLength)}></div>
                 } else {
                     return <div role="cell" key={key} className="cal-cel" onClick={() => console.log(key)}></div>
                 }
@@ -60,9 +57,9 @@ const Calendar = ({events}: any) => {
     }
 
     const renderEventDetail = (index: number) => {
-        if(selectDay.firstOfMonthIdx !== null) {
-            if(currentMonthEvents[index - selectDay.firstOfMonthIdx + 1]) {
-                setSelectEvent(currentMonthEvents[index - selectDay.firstOfMonthIdx + 1])
+        if(firstOfMonthIdx !== null) {
+            if(currentMonthEvents[index - firstOfMonthIdx + 1]) {
+                setSelectEvent(currentMonthEvents[index - firstOfMonthIdx + 1])
               } else {
                 setSelectEvent(null);
               }
@@ -70,12 +67,12 @@ const Calendar = ({events}: any) => {
     }
     
     const prevMonth = () => {
-        setSelectDay(initialSelectDayFormat(format(sub(new Date(`${selectDay.month}/${selectDay.day}/${selectDay.year}`), {months:1}), "MM/dd/yyyy")))
+        setSelectDay(initialSelectDayFormat(format(sub(new Date(`${month}/${day}/${year}`), {months:1}), "MM/dd/yyyy")))
         setSelectEvent(null);
     }
     
     const nextMonth = () => {
-        setSelectDay(initialSelectDayFormat(format(add(new Date(`${selectDay.month}/${selectDay.day}/${selectDay.year}`), {months:1}), "MM/dd/yyyy")))
+        setSelectDay(initialSelectDayFormat(format(add(new Date(`${month}/${day}/${year}`), {months:1}), "MM/dd/yyyy")))
         setSelectEvent(null);
     }
     
@@ -84,7 +81,7 @@ const Calendar = ({events}: any) => {
             <div className="cal-wrapper">
                 <div className="header">
                     <button style={{height: "25%"}} onClick={() => prevMonth()}>{'<'}</button>
-                    <h1>{months[Number(selectDay.month)]} {selectDay.year}</h1>
+                    <h1>{months[Number(month)]} {year}</h1>
                     <button style={{height: "25%"}} onClick={() => nextMonth()}>{'>'}</button>
                 </div>
                 <div className="cal-table">
